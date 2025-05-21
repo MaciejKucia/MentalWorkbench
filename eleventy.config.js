ad3fbeadf3ce301b37be210979fe9d02263b8b86
Matt Kucia <matt@kucia.net>
2025-05-22 00:37:59 +0200

    Add ids to headers

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,9 @@
 import eleventyPluginMathjax from "eleventy-plugin-mathjax";
+import { IdAttributePlugin } from "@11ty/eleventy";
 
 export default async function(eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyPluginMathjax);
+	eleventyConfig.addPlugin(IdAttributePlugin);
 	eleventyConfig.addPassthroughCopy("style.css");
 	eleventyConfig.addPassthroughCopy("logo.png");
 	eleventyConfig.addPassthroughCopy("lib/**/*.jpg");
