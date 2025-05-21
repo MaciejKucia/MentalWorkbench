c96f8a8955473910216a84d04b490cd918653661
Matt Kucia <matt@kucia.net>
2025-05-21 11:16:55 +0200

    fu

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,8 @@
 export default async function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("style.css");
 	eleventyConfig.addPassthroughCopy("logo.png");
-	eleventyConfig.addPassthroughCopy("**/*.jpg");
+	eleventyConfig.addPassthroughCopy("lib/**/*.jpg");
+	eleventyConfig.addPassthroughCopy("lib/**/*.png");
+	eleventyConfig.addPassthroughCopy("projects/**/*.jpg");
+	eleventyConfig.addPassthroughCopy("projects/**/*.png");
 };
