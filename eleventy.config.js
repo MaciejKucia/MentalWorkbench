7245390e87cea8de0f580d05d6bf14a0126da93f
Matt Kucia <matt@kucia.net>
2025-05-21 14:39:34 +0200

    Add support for mathjax

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,7 @@
+import eleventyPluginMathjax from "eleventy-plugin-mathjax";
+
 export default async function(eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyPluginMathjax);
 	eleventyConfig.addPassthroughCopy("style.css");
 	eleventyConfig.addPassthroughCopy("logo.png");
 	eleventyConfig.addPassthroughCopy("lib/**/*.jpg");
